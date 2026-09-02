2e848ebc3a04aaaef1c575810ce5560c6663f019
rckwd <key.grunge@gmail.com>
2026-09-02 12:38:04 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'ngabret-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/SR-FOOTWEAR-Creative-Control-Dashboard.html',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch dari cache saat offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
